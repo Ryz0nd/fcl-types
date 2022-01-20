12cb2bcc88bfd85fd027f6fc240d7637bc5e37ec
Dan <ryzond6728@gmail.com>
2022-01-20 16:47:15 +0900

    update fcl types

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,7 +6,7 @@ declare module "@onflow/fcl" {
    *
    * Once the wallet provider has authenticated the user, FCL will set the values on the current user object for future use and authorization.
    */
-  export function authenticate({ service, redir }: Authenticate): void;
+  export function authenticate(opts?: Authenticate): void;
   /**
    * Logs out the current user and sets the values on the current user object to null.
    */
@@ -34,7 +34,7 @@ declare module "@onflow/fcl" {
   /**
    * Holds the current user, if set, and offers a set of functions to manage the authentication and authorization of the user.
    */
-  export const currentUser: CurrentUser;
+  export function currentUser(): CurrentUser;
   /**
    * An alternative to Discovery Wallet (a pre-built service list for authentication)
    * where dapp developers can access services in directly in their code in order to build customized authentication experiences.
@@ -130,7 +130,7 @@ declare module "@onflow/fcl" {
    */
   export function getEventsAtBlockIds(
     eventName: EventName,
-    blockIds?: number[]
+    blockIds?: string[]
   ): Promise<EventObject[]>;
   /**
    * A builder function that returns all a collection containing a list of transaction ids by its collection id.
@@ -270,8 +270,8 @@ declare module "@onflow/fcl" {
 
   /**
    * A valid Flow address should be 16 characters in length.
-     A `0x` prefix is optional during inputs.
-     `eg. f8d6e0586b0a20c1`
+   * A `0x` prefix is optional during inputs.
+   * `eg. f8d6e0586b0a20c1`
    */
   export type Address = string;
   /**
@@ -392,11 +392,32 @@ declare module "@onflow/fcl" {
   }
 
   interface CurrentUser {
+    /**
+     * The callback passed to subscribe will be called when the user authenticates and un-authenticates,
+     * making it easy to update the UI accordingly.
+     */
     subscribe: (callback: (value: CurrentUserObject) => void) => void;
-    snapshot: CurrentUserObject;
-    authenticate: ({ service, redir }: Authenticate) => void;
+    /**
+     * Returns the current user object.
+     *
+     * This is the same object that is set and available on `fcl.currentUser().subscribe(callback)`.
+     */
+    snapshot: () => CurrentUserObject;
+    /**
+     * Equivalent to `fcl.authenticate`.
+     */
+    authenticate: (opts?: Authenticate) => void;
+    /**
+     * Equivalent to `fcl.unauthenticate`.
+     */
     unauthenticate: () => void;
+    /**
+     * Equivalent to `fcl.authz`.
+     */
     authorization: AuthorizationObject;
+    /**
+     * A method to use allowing the user to personally sign data via FCL Compatible Wallets/Services.
+     */
     signUserMessage: (message: string) => Promise<CompositeSignature>;
   }
 
@@ -426,29 +447,156 @@ declare module "@onflow/fcl" {
     authorizations?: AuthorizationObject[];
   }
 
-  /**
-   * 0: `Unknown`
-   *
-   * 1: `Transaction Pending - Awaiting Finalization`
-   *
-   * 2: `Transaction Finalized - Awaiting Execution`
-   *
-   * 3: `Transaction Executed - Awaiting Sealing`
-   *
-   * 4: `Transaction Sealed - Transaction Complete. At this point the transaction result has been committed to the blockchain.`
-   *
-   * 5: `Transaction Expired`
-   */
-  enum TransactionStatusCode {
-    "Unknown" = 0,
-    "Pending" = 1,
-    "Finalized" = 2,
-    "Executed" = 3,
-    "Sealed" = 4,
-    "Expired" = 5,
-  }
+  type TransactionStatusCode =
+    /**
+     * Unknown
+     */
+    | 0
+    /**
+     * Transaction Pending - Awaiting Finalization
+     */
+    | 1
+    /**
+     * Transaction Finalized - Awaiting Execution
+     */
+    | 2
+    /**
+     * Transaction Executed - Awaiting Sealing
+     */
+    | 3
+    /**
+     * Transaction Sealed - Transaction Complete. At this point the transaction
+     * result has been committed to the blockchain.
+     */
+    | 4
+    /**
+     * Transaction Expired
+     */
+    | 5;
 
-  type GRPCStatus = any;
+  type GRPCStatus =
+    /**
+     * OK - Not an error; returned on success.
+     */
+    | 0
+    /**
+     * CANCELLED - 	The operation was cancelled, typically by the caller.
+     */
+    | 1
+    /**
+     * UNKNOWN - Unknown error. For example, this error may be returned when a
+     * Status value received from another address space belongs to an error
+     * space that is not known in this address space. Also errors raised by APIs
+     * that do  not return enough error information may be converted to this
+     * error.
+     */
+    | 2
+    /**
+     * INVALID_ARGUMENT - The client specified an invalid argument. Note that
+     * this differs from FAILED_PRECONDITION. INVALID_ARGUMENT indicates
+     * arguments that are problematic regardless of the state of the system
+     * (e.g., a malformed file name).
+     */
+    | 3
+    /**
+     * DEADLINE_EXCEEDED - The deadline expired before the operation could
+     * complete. For operations that change the state of the system, this error
+     * may be returned even if the operation has completed successfully. For
+     * example, a successful response from a server could have been delayed
+     * long.
+     */
+    | 4
+    /**
+     * NOT_FOUND - Some requested entity (e.g., file or directory) was not
+     * found. Note to server developers: if a request is denied for an entire
+     * class of users, such as gradual feature rollout or undocumented
+     * allowlist, NOT_FOUND may be used. If a request is denied for some users
+     * within a class of users, such as user-based access control,
+     * PERMISSION_DENIED must be used.
+     */
+    | 5
+    /**
+     * ALREADY_EXISTS - The entity that a client attempted to create (e.g., file
+     * or directory) already exists.
+     */
+    | 6
+    /**
+     * PERMISSION_DENIED - The caller does not have permission to execute the
+     * specified operation. PERMISSION_DENIED must not be used for rejections
+     * caused by exhausting some resource (use RESOURCE_EXHAUSTED instead for
+     * those errors). PERMISSION_DENIED must not be used if the caller can not
+     * be identified (use UNAUTHENTICATED instead for those errors). This error
+     * code does not imply the request is valid or the requested entity exists
+     * or satisfies other pre-conditions.
+     */
+    | 7
+    /**
+     * RESOURCE_EXHAUSTED - Some resource has been exhausted, perhaps a per-user
+     * quota, or perhaps the entire file system is out of space.
+     */
+    | 8
+    /**
+     * FAILED_PRECONDITION - The operation was rejected because the system is
+     * not in a state required for the operation's execution. For example, the
+     * directory to be deleted is non-empty, an rmdir operation is applied to a
+     * non-directory, etc. Service implementors can use the following guidelines
+     * to decide between FAILED_PRECONDITION, ABORTED, and UNAVAILABLE: (a) Use
+     * UNAVAILABLE if the client can retry just the failing call. (b) Use
+     * ABORTED if the client should retry at a higher level (e.g., when a
+     * client-specified test-and-set fails, indicating the client should restart
+     * a read-modify-write sequence). (c) Use FAILED_PRECONDITION if the client
+     * should not retry until the system state has been explicitly fixed. E.g.,
+     * if an "rmdir" fails because the directory is non-empty,
+     * FAILED_PRECONDITION should be returned since the client should not retry
+     * unless the files are deleted from the directory.
+     */
+    | 9
+    /**
+     * ABORTED - The operation was aborted, typically due to a concurrency issue
+     * such as a sequencer check failure or transaction abort. See the
+     * guidelines above for deciding between FAILED_PRECONDITION, ABORTED, and
+     * UNAVAILABLE.
+     */
+    | 10
+    /**
+     * OUT_OF_RANGE - The operation was attempted past the valid range. E.g.,
+     * seeking or reading past end-of-file. Unlike INVALID_ARGUMENT, this error
+     * indicates a problem that may be fixed if the system state changes. For
+     * example, a 32-bit file system will generate INVALID_ARGUMENT if asked to
+     * read at an offset that is not in the range [0,2^32-1], but it will
+     * generate OUT_OF_RANGE if asked to read from an offset past the current
+     * file size. There is a fair bit of overlap between FAILED_PRECONDITION and
+     * OUT_OF_RANGE. We recommend using OUT_OF_RANGE (the more specific error)
+     * when it applies so that callers who are iterating through a space can
+     * easily look for an OUT_OF_RANGE error to detect when they are done.
+     */
+    | 11
+    /**
+     * UNIMPLEMENTED - The operation is not implemented or is not
+     * supported/enabled in this service.
+     */
+    | 12
+    /**
+     * INTERNAL - Internal errors. This means that some invariants expected by
+     * the underlying system have been broken. This error code is reserved for
+     * serious errors.
+     */
+    | 13
+    /**
+     * UNAVAILABLE - The service is currently unavailable. This is most likely a
+     * transient condition, which can be corrected by retrying with a backoff.
+     * Note that it is not always safe to retry non-idempotent operations.
+     */
+    | 14
+    /**
+     * DATA_LOSS - Unrecoverable data loss or corruption.
+     */
+    | 15
+    /**
+     * UNAUTHENTICATED - The request does not have valid authentication
+     * credentials for the operation.
+     */
+    | 16;
 
   export interface TransactionStatus {
     /**
