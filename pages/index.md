9bb757cf568932507e5af788e64b9b8304c76836
Junho Yeo <i@junho.io>
2022-06-30 07:32:43 +0900

    Generate docs with Nextra + typedoc-plugin-markdown

--- pages/index.md
+++ pages/index.md
@@ -1,3 +1,5 @@
+FCL Types - v0.0.5 / [Exports](/docs/modules.md)
+
 # fcl-types
 
 The repository for type definitions of [`@onflow/fcl`](https://github.com/onflow/fcl-js) library.
---
+++ theme.config.js
@@ -0,0 +1,25 @@
+export default {
+  projectLink: 'https://github.com/shuding/nextra',
+  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
+  titleSuffix: ' – FCL Types',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © Alphaworks.`,
+  footerEditLink: `Edit this page on GitHub`,
+  logo: (
+    <>
+      <svg>...</svg>
+      <span>FCL Types</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next docs builder" />
+      <meta name="og:title" content="Nextra: the next docs builder" />
+    </>
+  ),
+};
